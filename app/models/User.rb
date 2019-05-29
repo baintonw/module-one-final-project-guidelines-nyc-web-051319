803d44ec49915cbd731560874f3e7c61745209ab
marycriv <maryecrivelli@gmail.com>
2019-05-29 09:14:33 -0400

    Updated with Will's methods

--- app/models/User.rb
+++ app/models/User.rb
@@ -8,13 +8,21 @@ class User < ActiveRecord::Base
   end #Works √
 
 
-  def watch_episode
+  def watch_episode(episode_id, rating)
     # Creates new instance of view
     # Watches a single episode of a show
-    # Should this take in an instance of an Episode, and connect that episode instance through User-View-Episode? 
     # Should this automatically ask for rating?
     # How does id of view connect to show?
-    View.create
+    View.create(user_id: self.id, episode_id: episode_id, rating: rating)
+
+  end
+
+  def rate(episode_id, new_rating)
+    # Updates rate for episode
+    # Iterate through
+    episode = select_episode(episode_id)
+
+    episode.rating = new_rating
   end
 
   def hot_list
@@ -22,14 +30,15 @@ class User < ActiveRecord::Base
     # Should watching the first ep of a show auto-add it to queue? How to define first ep of a show?
   end
 
+  def remove_view
+  end
+
   def finish_episode # Change name to finale episode/finish show?
     # Complete show (removes show from queue)
     # Maybe: moves show from 'queue' to 'watch list'
-  end
-
-  def rate
-    # Rates show
-    # Should this be an initialize/automatic action?
+    ###selet episode returns episode ids, return specific episode ids that match user ids####
+    ####returns a list of viewed episodes####
+    ##maybe add to watched list###
   end
 
   def most_popular
@@ -37,5 +46,11 @@ class User < ActiveRecord::Base
     # Find most watched shows of all time, or find show that I (user) have watched the most episodes of?
     # Should this method be held in a different class, like show or episode/whatever "show" class belongs to?
   end
+
+  def select_episode(ep_id)
+    View.all.select do |ep|
+      ep.episode_id = ep_id
+    end
+  end # Select by episode helper method
 
 end # End user class
