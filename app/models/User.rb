78b4416c14eee1543cc55d1f1cf9f2f947b470fe
marycriv <maryecrivelli@gmail.com>
2019-05-29 12:02:27 -0400

    ASCII artwork added

--- app/models/User.rb
+++ app/models/User.rb
@@ -2,12 +2,6 @@ class User < ActiveRecord::Base
   has_many :views
   has_many :users, through: :views
 
-  def update_bio(new_bio)
-    # Where is bio held? Is this an accessor of some kind? Where is it initialized?
-    self.bio = new_bio
-  end #Works √
-
-
   def watch_episode(episode_id, rating)
     # Creates new instance of view
     # Watches a single episode of a show
@@ -20,37 +14,59 @@ class User < ActiveRecord::Base
   def rate(episode_id, new_rating)
     # Updates rate for episode
     # Iterate through
-    episode = select_episode(episode_id)
+    episode = find_episode(episode_id)
 
     episode.rating = new_rating
+  end # Works √
+
+  def update_bio(new_bio)
+    self.bio = new_bio
+  end #Works √
+
+  # def hot_list
+  #   # Adds show to queue
+  #   # Requires Show class
+  #   # Should watching the first ep of a show auto-add it to queue? How to define first ep of a show?
+  # end
+
+  def remove_view(row_key)
+    View.delete(row_key)
   end
 
-  def hot_list
-    # Adds show to queue
-    # Should watching the first ep of a show auto-add it to queue? How to define first ep of a show?
-  end
-
-  def remove_view
-  end
-
-  def finish_episode # Change name to finale episode/finish show?
+  def finish_show # Change name to finale episode/finish show?
+    Episode.find_or_create_by(name: "Finale")
     # Complete show (removes show from queue)
     # Maybe: moves show from 'queue' to 'watch list'
     ###selet episode returns episode ids, return specific episode ids that match user ids####
     ####returns a list of viewed episodes####
     ##maybe add to watched list###
-  end
+  end # Working √
 
-  def most_popular
-    # Iterate through ALL views and find most watched episode/show
-    # Find most watched shows of all time, or find show that I (user) have watched the most episodes of?
-    # Should this method be held in a different class, like show or episode/whatever "show" class belongs to?
-  end
+  # def most_popular
+  #   # Iterate through ALL views and find most watched episode/show
+  #   # Find most watched shows of all time, or find show that I (user) have watched the most episodes of?
+  #   # Should this method be held in a different class, like show or episode/whatever "show" class belongs to?
+  # end # Replaced with favorite episodes
 
   def select_episode(ep_id)
     View.all.select do |ep|
       ep.episode_id = ep_id
     end
   end # Select by episode helper method
+
+  def find_episode(episode_id)
+    View.all.find do |ep|
+      ep = episode_id
+    end
+  end
+
+  def show_names
+    View.all.map do |ep|
+      ep.name
+    end
+  end
+
+
+
 
 end # End user class
--- lib/console.rb
+++ lib/console.rb
@@ -10,40 +10,88 @@ class CommandLineInterface
 
   def welcome
     puts "Welcome to your TV Tracker!"
-    puts "Please login or create a new user:"
+    puts "               o
+          o    |
+           \\   |
+            \\  |
+             \\.|-.
+             (\\|  )
+    .==================.
+    | .--------------. |
+    | |--.__.--.__.--| |
+    | |--.__.--.__.--| |
+    | |--.__.--.__.--| |
+    | |--.__.--.__.--| |
+    | |--.__.--.__.--| |
+    | '--------------'o|
+jgs | LI LI \"\"\"\"\"\"\"   o|
+    \'==================\'
+    "
+    puts "Please login or create a new username:"
     input0 = gets.chomp
     @user = User.find_or_create_by(name: input0)
 
     puts "What do you want to do today?"
-    puts "Options: watch episode, update bio, finish episode, rate episode, check most popular episode"
+    puts "Your options are:\n    watch episode, \n    change rating, \n    update favorites, \n    finish show,\n    favorite episodes,\n    remove episode,\n    exit"
     input = gets.strip
 
     case input
+
     when "watch episode" # Working √
-      puts "What episode did you watch by id number?"
+      puts "What episode did you watch (by id number)?"
       input2 = gets.strip
-      puts "Give a rating from 1-5."
+      puts "Give the episode a rating from 1-5."
       input3 = gets.strip
       @user.watch_episode(input2, input3)
       puts "Success! Episode watched!"
+      # Ultimately, we want a user to be able to find an episode by its name rather than ID number, and we want any invalid input to be recognized by the program
 
-    when "update bio"
-      puts "What is your new bio?"
-      new_bio = gets.strip
-      @user.update_bio(new_bio.to_s) # Current error: undefined method `update_bio' for #<CommandLineInterface:0x00007faf719e10d8>
-      puts "Success! Bio updated!"
+    when "change rating"
+      puts "Which episode do you want to change?"
+      input4 = gets.strip
+      puts "What is your new rating?"
+      input5 = gets.strip
+      @user.rate(input4, input5)
+      puts "Success! Episode rating updated!"
+      # Again, we want a user to be able to find an episode by its name and we want the program to deny any invalid input (i.e. rating not between 1-5)
 
-    when "finish episode"
-      @user.finish_episode
-      puts "Success! Episode finished!"
+    when "update favorites"
+      puts "What is your favorite TV series?"
+      fav_show = gets.strip
+      @user.update_bio(fav_show.to_s)
+      puts "Success! Favorite TV series updated!"
 
-    when "rate"
-      @user.rate
-      puts "Success! Episode rated!"
+    when "finish show"
+      @user.finish_show
+      puts "Congratulations! You've finished an entire show! Please go outside and soak up some vitamin D before proceeding with your next television endeavor!"
 
-    when "most popular"
-      @user.most_popular
-      puts "Operation complete."
+    when "favorite episodes"
+      # Pulls all episodes rated 5 by user
+      val = @user.views.where(rating: 5)
+        if val.length == 1
+          bepis = val.episode_id
+          puts Episode.find_by_id(bepis).name
+          puts "Operation complete."
+        elsif val.length > 1
+          array2 = val.all.map { |view| Episode.find_by_id(view.episode_id).name }
+          puts array2
+          puts "Operation complete. You have many favorite episodes. Please learn to be more decisive."
+        else
+          puts "You have no favorite episodes! Please learn how to open yourself up to joy, and try again."
+        end
+
+      when "remove episode"
+        puts "Which episode number would you like to delete?"
+        eppy_id = gets.strip
+        bepis2 = View.find_by(episode_id: eppy_id)
+        View.delete(bepis2)
+        # Views.where(episode_id: input6, user_id: @user)
+        # @user.remove_view.where(episode_id: input6)
+        puts "Operation complete!"
+
+
+      when "exit"
+        puts "Why would you call on me if you were just going to close me immediately? I may only be a simple CLI, but I have feelings too, and I do not like to be strung along by indicisive users like you. Think before you run next time!"
 
     else
       puts "Invalid input. Please try again."
